18da06a98a5ca4907fb12d4c4446635f4577e46d
SrAbrazos <64601588+SrAbrazos@users.noreply.github.com>
2020-05-10 20:53:36 -0400

    Add files via upload

--- Proyecto1.m
+++ Proyecto1.m
@@ -1,9 +1,9 @@
 %% Proyecto 1
 % Profesor: Claudio Held
 % Estudiante: Matías Osses
-% Estudiante: 
+% Estudiante: Alvaro Toledo
 
-%% Condiciones Iniciales
+%% Condicion Inicial P=650
 
 PO = 750;
 P = 650; %Presión inicial
@@ -16,7 +16,8 @@ Error = zeros(1,t);
 TasaError = zeros(1,t);
 Presion = zeros(1,t);
 Calor = zeros(1,t);
-
+reglas1=zeros(1,17);
+reglas2=zeros(1,17);
 for i=1:t
    %Se guarda los valores actuales de cada variable
    Error(1,i) = EP;
@@ -33,8 +34,167 @@ for i=1:t
    TP = TP/15;
    
    %CLD
-   SS = inferencia(EP, TP);
-   dH = desdifusion(SS);
+   [SS,a,b]= inferencia(EP, TP);
+  
+   reglas1=a+reglas1;
+   reglas2=b+reglas2;
+
+   dH = desdifusion(SS,'CG');
+   
+   %desfuzzificación
+   dH = dH*15;
+   EP = EP*15;
+   
+   %Actualización
+   P = P + 0.7*dH;
+   aux = P - PO;
+   TP = aux - EP;
+   EP = aux;
+   
+end
+
+t = 1:200;
+
+figure(1)
+bar(reglas1)
+title('Evolución de la secuencia de reglas 1 P0=650 y defuzz CG')
+
+figure(2)
+bar(reglas2)
+title('Evolución de la secuencia de reglas 2 P0=650 y defuzz CG')
+
+figure(3)
+plot(t,Error)
+title('Evolución del Error con P0=650 y defuzz CG')
+
+figure(4)
+plot(t,TasaError)
+title('Evolución Tasa del Error con P0=650 y defuzz CG')
+    
+figure(5)
+plot(t,Presion)
+title('Evolución de la Presion con P0=650 y defuzz CG')
+
+figure(6)
+plot(t,Calor)
+title('Evolución del Calor con P0=650 y defuzz CG')
+
+
+PO = 750;
+P = 650; %Presión inicial
+EP = P - PO; %Error inicial
+TP = 0; %Tasa de error inicial
+dH = 0; %Delta calor
+
+t = 200;
+Error = zeros(1,t);
+TasaError = zeros(1,t);
+Presion = zeros(1,t);
+Calor = zeros(1,t);
+reglas1=zeros(1,17);
+reglas2=zeros(1,17);
+areas1=zeros(1,17);
+areas2=zeros(1,17);
+for i=1:t
+   %Se guarda los valores actuales de cada variable
+   Error(1,i) = EP;
+   TasaError(1,i)= TP;
+   Presion(1,i) = P;
+   Calor(1,i) = dH;
+   
+   %Sensor
+   EP = saturacion(EP);
+   TP = saturacion(TP);
+   
+   %Fuzzificación
+   EP = EP/15;
+   TP = TP/15;
+   
+   %CLD
+   [SS,a,b,c,d]= inferencia(EP, TP);
+   
+   reglas1=a+reglas1;
+   reglas2=b+reglas2;
+   areas1=abs(c)+areas1;
+   areas2=abs(d)+areas2;
+
+   dH = desdifusion(SS,'MoM');
+   
+   %desfuzzificación
+   dH = dH*15;
+   EP = EP*15;
+   
+   %Actualización
+   P = P + 0.7*dH;
+   aux = P - PO;
+   TP = aux - EP;
+   EP = aux;
+   
+end
+
+t = 1:200;
+
+
+figure(7)
+bar(reglas1)
+title('Evolución de la secuencia de reglas 1 P0=650 y defuzz MoM')
+
+figure(8)
+bar(reglas2)
+title('Evolución de la secuencia de reglas 2 P0=650 y defuzz MoM')
+
+figure(9)
+plot(t,Error)
+title('Evolución del Error con P0=650 y defuzz MoM')
+
+figure(10)
+plot(t,TasaError)
+title('Evolución Tasa del Error con P0=650 y defuzz MoM')
+
+figure(11)
+plot(t,Presion)
+title('Evolución de la Presion con P0=650 y defuzz MoM')
+
+figure(12)
+plot(t,Calor)
+title('Evolución del Calor con P0=650 y defuzz MoM')
+f
+
+%%
+%%Condicion Inicial P=730
+PO = 750;
+P = 730; %Presión inicial
+EP = P - PO; %Error inicial
+TP = 0; %Tasa de error inicial
+dH = 0; %Delta calor
+
+t = 200;
+Error = zeros(1,t);
+TasaError = zeros(1,t);
+Presion = zeros(1,t);
+Calor = zeros(1,t);
+reglas1=zeros(1,17);
+reglas2=zeros(1,17);
+for i=1:t
+   %Se guarda los valores actuales de cada variable
+   Error(1,i) = EP;
+   TasaError(1,i)= TP;
+   Presion(1,i) = P;
+   Calor(1,i) = dH;
+   
+   %Sensor
+   EP = saturacion(EP);
+   TP = saturacion(TP);
+   
+   %Fuzzificación
+   EP = EP/15;
+   TP = TP/15;
+   
+   %CLD
+   [SS,a,b]= inferencia(EP, TP);
+   reglas1=a+reglas1;
+   reglas2=b+reglas2;
+   dH = desdifusion(SS,'CG');
    
    %desfuzzificación
    dH = dH*15;
@@ -49,18 +209,241 @@ end
 
 t = 1:200;
 
-figure(1)
+figure(13)
+bar(reglas1)
+title('Evolución de la secuencia de reglas 1 P0=730 y defuzz CG')
+
+figure(14)
+bar(reglas2)
+title('Evolución de la secuencia de reglas 2 P0=730 y defuzz CG')
+
+figure(15)
 plot(t,Error)
-title('Evolución del Error')
+title('Evolución del Error con P0=730 y defuzz CG')
 
-figure(2)
+figure(16)
 plot(t,TasaError)
-title('Evolución Tasa del Error')
+title('Evolución Tasa del Error con P0=730 y defuzz CG')
 
-figure(3)
+figure(17)
 plot(t,Presion)
-title('Evolución de la Presion')
+title('Evolución de la Presion con P0=730 y defuzz CG')
 
-figure(4)
+figure(18)
 plot(t,Calor)
-title('Evolución del Calor')
+title('Evolución del Calor con P0=730 y defuzz CG')
+
+PO = 750;
+P = 730; %Presión inicial
+EP = P - PO; %Error inicial
+TP = 0; %Tasa de error inicial
+dH = 0; %Delta calor
+
+t = 200;
+Error = zeros(1,t);
+TasaError = zeros(1,t);
+Presion = zeros(1,t);
+Calor = zeros(1,t);
+reglas1=zeros(1,17);
+reglas2=zeros(1,17);
+for i=1:t
+   %Se guarda los valores actuales de cada variable
+   Error(1,i) = EP;
+   TasaError(1,i)= TP;
+   Presion(1,i) = P;
+   Calor(1,i) = dH;
+   
+   %Sensor
+   EP = saturacion(EP);
+   TP = saturacion(TP);
+   
+   %Fuzzificación
+   EP = EP/15;
+   TP = TP/15;
+   
+   %CLD
+   [SS,a,b]= inferencia(EP, TP);
+   reglas1=a+reglas1;
+   reglas2=b+reglas2;
+   dH = desdifusion(SS,'MoM');
+   %desfuzzificación
+   dH = dH*15;
+   EP = EP*15;
+   
+   %Actualización
+   P = P + 0.7*dH;
+   aux = P - PO;
+   TP = aux - EP;
+   EP = aux;
+end
+
+t = 1:200;
+
+figure(19)
+bar(reglas1)
+title('Evolución de la secuencia de reglas 1 P0=730 y defuzz MoM')
+
+figure(20)
+bar(reglas2)
+title('Evolución de la secuencia de reglas 2 P0=730 y defuzz MoM')
+
+figure(21)
+plot(t,Error)
+title('Evolución del Error con P0=730 y defuzz MoM')
+
+figure(22)
+plot(t,TasaError)
+title('Evolución Tasa del Error con P0=730 y defuzz MoM')
+
+figure(23)
+plot(t,Presion)
+title('Evolución de la Presion con P0=730 y defuzz MoM')
+
+figure(24)
+plot(t,Calor)
+title('Evolución del Calor con P0=730 y defuzz MoM')
+
+%%
+%%Condicion Inicial P=840
+PO = 750;
+P = 840; %Presión inicial
+EP = P - PO; %Error inicial
+TP = 0; %Tasa de error inicial
+dH = 0; %Delta calor
+
+t = 200;
+Error = zeros(1,t);
+TasaError = zeros(1,t);
+Presion = zeros(1,t);
+Calor = zeros(1,t);
+reglas1=zeros(1,17);
+reglas2=zeros(1,17);
+for i=1:t
+   %Se guarda los valores actuales de cada variable
+   Error(1,i) = EP;
+   TasaError(1,i)= TP;
+   Presion(1,i) = P;
+   Calor(1,i) = dH;
+   
+   %Sensor
+   EP = saturacion(EP);
+   TP = saturacion(TP);
+   
+   %Fuzzificación
+   EP = EP/15;
+   TP = TP/15;
+   
+   %CLD
+   [SS,a,b]= inferencia(EP, TP);
+   reglas1=a+reglas1;
+   reglas2=b+reglas2;
+   dH = desdifusion(SS,'CG');
+   
+   %desfuzzificación
+   dH = dH*15;
+   EP = EP*15;
+   
+   %Actualización
+   P = P + 0.7*dH;
+   aux = P - PO;
+   TP = aux - EP;
+   EP = aux;
+end
+
+t = 1:200;
+
+figure(25)
+bar(reglas1)
+title('Evolución de la secuencia de reglas 1 P0=840 y defuzz CG')
+
+figure(26)
+bar(reglas2)
+title('Evolución de la secuencia de reglas 2 P0=840 y defuzz CG')
+
+figure(27)
+plot(t,Error)
+title('Evolución del Error con P0=840 y defuzz CG')
+
+figure(28)
+plot(t,TasaError)
+title('Evolución Tasa del Error con P0=840 y defuzz CG')
+
+figure(29)
+plot(t,Presion)
+title('Evolución de la Presion con P0=840 y defuzz CG')
+
+figure(30)
+plot(t,Calor)
+title('Evolución del Calor con P0=840 y defuzz CG')
+
+%%Condicion Inicial P=840
+PO = 750;
+P = 840; %Presión inicial
+EP = P - PO; %Error inicial
+TP = 0; %Tasa de error inicial
+dH = 0; %Delta calor
+
+t = 200;
+Error = zeros(1,t);
+TasaError = zeros(1,t);
+Presion = zeros(1,t);
+Calor = zeros(1,t);
+reglas1=zeros(1,17);
+reglas2=zeros(1,17);
+for i=1:t
+   %Se guarda los valores actuales de cada variable
+   Error(1,i) = EP;
+   TasaError(1,i)= TP;
+   Presion(1,i) = P;
+   Calor(1,i) = dH;
+   
+   %Sensor
+   EP = saturacion(EP);
+   TP = saturacion(TP);
+   
+   %Fuzzificación
+   EP = EP/15;
+   TP = TP/15;
+   
+   %CLD
+   [SS,a,b]= inferencia(EP, TP);
+   reglas1=a+reglas1;
+   reglas2=b+reglas2;
+   dH = desdifusion(SS,'MoM');
+   
+   %desfuzzificación
+   dH = dH*15;
+   EP = EP*15;
+   
+   %Actualización
+   P = P + 0.7*dH;
+   aux = P - PO;
+   TP = aux - EP;
+   EP = aux;
+end
+
+t = 1:200;
+
+figure(31)
+bar(reglas1)
+title('Evolución de la secuencia de reglas 1 P0=840 y defuzz MoM')
+
+figure(32)
+bar(reglas2)
+title('Evolución de la secuencia de reglas 2 P0=840 y defuzz MoM')
+
+figure(33)
+plot(t,Error)
+title('Evolución del Error con P0=840 y defuzz MoM')
+
+figure(34)
+plot(t,TasaError)
+title('Evolución Tasa del Error con P0=840 y defuzz MoM')
+
+figure(35)
+plot(t,Presion)
+title('Evolución de la Presion con P0=840 y defuzz MoM')
+
+figure(36)
+plot(t,Calor)
+title('Evolución del Calor con P0=840 y defuzz MoM')
